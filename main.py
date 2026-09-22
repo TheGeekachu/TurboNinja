0e45e479e3d154a52139e4f32b3752f463a6ebe0
vijayakasee-debug <vijayakasee@gmail.com>
2026-09-22 12:46:37 -0700

    Level Transition

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import math
 import random
@@ -71,14 +72,25 @@ class Game:
         
         self.scroll = [0, 0]
         self.dead = 0
+        self.transition = -30
         
     def run(self):
         while True:
             self.display.blit(self.assets['background'], (0, 0))
             self.screenshake = max(0, self.screenshake - 1)
             
+            if not len(self.enemies):
+                self.transition += 1
+                if self.transition > 30:
+                    self.level = min(self.level + 1, len(os.listdir('data/maps')) - 1)
+                    self.load_level(self.level)
+            if self.transition < 0:
+                self.transition += 1
+            
             if self.dead:
                 self.dead += 1
+                if self.dead >= 10:
+                    self.transition = min(30, self.transition + 1)
                 if self.dead > 40:
                     self.load_level(self.level)
             
@@ -161,6 +173,13 @@ class Game:
                         self.movement[0] = False
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
+            
+            if self.transition:
+                transition_surf = pygame.Surface(self.display.get_size())
+                pygame.draw.circle(transition_surf, (255, 255, 255), (self.display.get_width() // 2, self.display.get_height() // 2), (30 - abs(self.transition)) * 8)
+                transition_surf.set_colorkey((255, 255, 255))
+                self.display.blit(transition_surf, (0, 0))
+
             screenshake_offset = (random.random() * self.screenshake - self.screenshake / 2, random.random() * self.screenshake - self.screenshake / 2)
             self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), screenshake_offset)
             pygame.display.update()
